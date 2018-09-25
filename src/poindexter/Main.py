2e14aca76cba2d9b737f6cee14a897187f7413af
Eduard <kuliev.ed@gmail.com>
2018-09-26 01:01:51 +0300

    Test deploing

--- src/poindexter/Main.py
+++ src/poindexter/Main.py
@@ -53,7 +53,7 @@ class VKBot:
 
 
 def start(message, vk):
-    vk.messages.send(user_id=message.user_id, message=u"Ну поехали")
+    vk.messages.send(user_id=message.user_id, message=u"Ну давай уже")
 
 
 def random_habrahabr(message, vk):
