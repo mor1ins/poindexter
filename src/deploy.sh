05041d621e26f353d50b7e82c2e3046b13a84fdf
Eduard <kuliev.ed@gmail.com>
2018-09-26 00:49:20 +0300

    деплой готов

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,11 +1,14 @@
 cd poindexter
-PID=$(python3 Main.py &)
+python3 Main.py &
+cd ..
 
-while true do
+while true
+do
         echo "hello"
         git remote update
         if git status | grep "is behind"
         then
+                PID=$(ps ux | grep -m1 "Main.py" | cut -d" " -f4)
                 kill $PID
                 echo "Kill $PID"
                 git pull
