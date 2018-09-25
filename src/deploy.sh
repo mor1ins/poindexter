ac9bd11ad02236fa34030add694e22d09d8e4a39
Eduard <kuliev.ed@gmail.com>
2018-09-26 00:34:18 +0300

    Тест деплоя

--- src/deploy.sh
+++ src/deploy.sh
@@ -1,14 +1,15 @@
 cd poindexter
-PID=$(python Main.py &)
+PID=$(python3 Main.py &)
 
-while true; do
+while true do
+        echo "hello"
         git remote update
         if git status | grep "is behind"
         then
                 kill $PID
                 echo "Kill $PID"
                 git pull
-                PID=$(python Main.py &)
+                PID=$(python3 Main.py &)
         fi
         sleep 10
 done
