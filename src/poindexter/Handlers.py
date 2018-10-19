bd96b16f532c8cd92de61ffe9c180afbfd20edd7
Eduard <kuliev.ed@gmail.com>
2018-10-20 02:41:22 +0300

    Немного порефакторили

--- src/poindexter/Handlers.py
+++ src/poindexter/Handlers.py
@@ -39,18 +39,20 @@ def vk_ready_download_handler(message):
 
     downloader = VkLoader()
     title, ext = downloader.process(message.user_id, local_dir)
+
+    if ext != "zip":
+        logger(user_id=message.user_id, log=u"Некорректный формат архиива")
+        return
+
     logger(user_id=message.user_id, log=u"Разархивировали")
     matches = re.findall(format_dir, title, re.U)
-    # if len(matches) < 0:
-    #     logger(user_id=message.user_id, log=u"Некорректное имя архива")
-    #     return
+    if len(matches) < 0:
+        logger(user_id=message.user_id, log=u"Некорректное имя архива")
+        return
 
     pdf_generator = PdfGenerator()
     pdf_generator.process(local_dir, title)
     logger(user_id=message.user_id, log=u"Пдф сгенерирована")
-
-    # doc = VkUploader().messages_save(local_dir % ("%s.%s" % (title, "pdf")), 'doc', message.user_id, title)
-    # view_api.vk.messages.send(peer_id=message.user_id, attachment='doc%s_%s' % (doc['owner_id'], doc['id']))
 
     global_db.insert_into(Note.fromList(matches[0]).__str__())
 
@@ -71,7 +73,6 @@ def vk_ready_download_handler(message):
     url = user_api.docs.getUploadServer(group_id=171785116, v='5.85')['upload_url']
     file_for_upload = local_dir % ("%s.%s" % ("note_pdf", "pdf"))
     if os.path.exists(file_for_upload):
-        # file = {title: open(file_for_upload, "rb")}
         resp = requests.post(url, files={'file': open(file_for_upload, "rb")}).json()
         doc = user_api.docs.save(file=resp['file'], title=title, v='5.85')[0]
         logger(user_id=message.user_id, log=u"Конспект успешно добавлен")
